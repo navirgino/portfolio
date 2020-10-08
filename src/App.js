d10a4ac8745760cccdda2bc1cb7c27fa27471b8d
Nico Virgino <nicovirgino@gmail.com>
2020-10-07 23:02:16 -0500

    feature: added a bounce effect for my image. in prelims of wireframe realization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import my_picture_professional from './my_headshot_professional.jpg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import { bounce } from 'react-animations';
+import styled, { keyframes } from 'styled-components';
+
+
+const Bounce= styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Bounce><Image src={my_picture_professional} className="App-logo" alt="logo" roundedCircle /></Bounce>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
